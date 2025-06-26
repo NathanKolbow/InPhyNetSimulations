b9d3fa714bf1b164547b3281c06a788f9c4c5a46
NathanKolbow <nathankolbow@gmail.com>
2025-06-26 13:51:23 -0500

    cleared new branch

--- helpers/file-locking.jl
+++
@@ -1,5 +0,0 @@
-using Pidfile
-
-function lock_file(out_file::String)
-    lk = mkpidlock(out_file*".lk")
-end
--- true-gts/condor/submit/snaq.tab.py
+++
@@ -1,7 +0,0 @@
-with open("snaq.tab", "r") as f:
-    lines = f.readlines()
-    n500r1_lines = [line for line in lines if line.split(",")[0] == "500" and (line.split(",")[1] == "1" or line.split(",")[1] == "2")]
-    
-    with open("snaq_n500r12.tab", "w+") as new_f:
-        for line in n500r1_lines:
-            new_f.write(line)
