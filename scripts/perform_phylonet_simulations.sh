d01757ac6ba0c4a8ba94fe3fcf506bc9cac98176
Nathan Kolbow <nkolbow@wisc.edu>
2025-07-25 09:15:43 -0500

    added phylonet scripts

---
+++ scripts/perform_phylonet_simulations.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+scriptdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+#for r in $(seq 1 10); do
+for r in $(seq 6 10); do
+for m in 10; do
+for ngt in 100 1000; do
+for ils in low high; do
+for nbp in 100 1000; do
+for n in 25 50 100 200; do
+    printf "\033[0;31m-----------------------------------------------------------------------------------\n"
+    printf "\033[0;31m>> ${scriptdir}/perform_simulation.sh $n $ngt $ils $nbp $m $r phylonet <<\n"
+    printf "\033[0;31m-----------------------------------------------------------------------------------\n\n\033[0m"
+    ${scriptdir}/perform_simulation.sh $n $ngt $ils $nbp $m $r phylonet
+done
+done
+done
+done
+done
+done
